f064c0ad75c841b8f42ac84b982846d4c522549d
milla <milla@LAPTOP-M77O6UDR>
2019-02-05 22:52:00 -0800

    Initial commit

---
+++ ImageGhange/scripts/JavaScript.js
@@ -0,0 +1,32 @@
+﻿var mypic;
+var img = qs("#inpt-img");
+
+
+img.on("load", function (e) {
+
+    mypic = new canvasUtils.Util(img, can);//getting a new istance of Util
+    mypic.paint();
+
+});
+
+img.attr("src", "img/foto9.jpg");
+
+
+
+
+
+//image upload
+function handleFiles(files) {
+    var reader = new FileReader();
+
+    var file = files[0];
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        console.error("Error while reading file");
+    }
+
+    reader.onload = function () {
+        img.src = reader.result;
+    }
+}
